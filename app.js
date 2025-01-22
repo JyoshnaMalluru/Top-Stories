89a5118ceced62c08e3e24f233a21a307bd21529
Jyoshna Malluru <jyoshna1595@gmail.com>
2025-01-22 23:11:45 +0530

    add changes

--- app.js
+++ app.js
@@ -12,19 +12,29 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"/public")));
 app.engine('ejs', ejsMate);
 
-// app.get("/",(req,res)=>{
-//     res.send("server is running");
-// })
-
 app.get("/",async(req,res)=>{
     const newsData = getNewsData();
-    if (newsData && newsData.articles) {
-        res.render("newspapers/index", { articles: newsData.articles });
+    if (newsData && newsData.data) {
+        res.render("newspapers/index", { data: newsData.data });
     } else {
-        console.log('No articles found or data not yet fetched.');
-        res.render("newspapers/index",{articles :[]});
+        res.render("newspapers/index",{data :[]});
     }
 });
+
+app.get("/:source",async(req,res)=>{
+    const {source} = req.params;
+    const newsData = getNewsData();
+    if (newsData && newsData.data) {
+        const filteredNews = newsData.data.filter(news => {
+            return news.source.trim().toLowerCase() === source.trim().toLowerCase();
+        });
+        if (filteredNews.length>0) {
+            res.render("newspapers/show", { data: filteredNews, name: source });
+        } else {
+            res.status(404).render("newspapers/error",{message: "No Data Found"});
+        }
+    }
+})
 
 app.listen(8080,()=> {
     console.log("listening to port");
--- init/server.js
+++ init/server.js
@@ -1,18 +1,17 @@
-const apiKey = "d23d2653e592412bb9f1866b7e61e222";
-const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
+// const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+const url = `http://api.mediastack.com/v1/news?access_key=${apiKey}&countries=in`
 let newsData = null;
 
 fetch(url)
     .then(response => response.json())
     .then(data =>{
         newsData = data;
-        //console.log(newsData);
         console.log("data successfully giving output");
     })
     .catch(error => {
         console.error('Error fetching news:', error);
-    });
-    //console.log(`API Key: ${apiKey}`);
-    
+    });    
     module.exports = { getNewsData: () => newsData };
 
